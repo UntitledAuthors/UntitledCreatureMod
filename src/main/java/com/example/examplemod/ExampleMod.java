eb1106aee664623e3a17c99653da5c3c5a2119cd
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2021-01-07 00:46:03 +0000

    Revert "broken"
    This reverts commit c046d32ac0109f14ed00f7d0854740e272757918.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,4 +1,4 @@
-package untitledcreaturemod;
+package com.example.examplemod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("untitledcreaturemod")
+@Mod("examplemod")
 public class ExampleMod
 {
     // Directly reference a log4j logger.
