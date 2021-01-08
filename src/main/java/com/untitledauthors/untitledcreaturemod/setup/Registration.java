04b1fffac83673437d9bcbdb63b0e967a5c25dfa
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2021-01-08 16:41:11 +0000

    Broken Registration file
    Two errors on line 24 and 41 in the setup.Registration.java file

--- src/main/java/com/untitledauthors/untitledcreaturemod/setup/Registration.java
+++ src/main/java/com/untitledauthors/untitledcreaturemod/setup/Registration.java
@@ -2,6 +2,8 @@ package com.untitledauthors.untitledcreaturemod.setup;
 
 import com.untitledauthors.untitledcreaturemod.creature.toad.ToadEntity;
 import com.untitledauthors.untitledcreaturemod.creature.toad.ToadEntityRenderer;
+import com.untitledauthors.untitledcreaturemod.items.ToadMobEggItem;
+import net.minecraft.item.Item;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
@@ -12,10 +14,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+
 import static com.untitledauthors.untitledcreaturemod.UntitledCreatureMod.MODID;
 
 public class Registration {
     private static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, MODID);
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static final RegistryObject<ToadMobEggItem> TOAD_EGG = ITEMS.register("toad_egg", ToadMobEggItem::new);
 
     public static final RegistryObject<EntityType<ToadEntity>> TOAD = ENTITIES.register("toad", () -> EntityType.Builder.create(ToadEntity::new, EntityClassification.CREATURE)
             .size(.8f, .4f)
