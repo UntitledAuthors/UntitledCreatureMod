e5c5802c5fd310a85b49c7dfdcd45f705974cfe8
Paspartout <Paspartout@users.noreply.github.com>
2021-01-25 18:37:26 +0100

    AntelopeWarHorn: Placeholder sound

--- src/main/java/com/untitledauthors/untitledcreaturemod/items/AntelopeWarHorn.java
+++ src/main/java/com/untitledauthors/untitledcreaturemod/items/AntelopeWarHorn.java
@@ -7,6 +7,8 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.world.World;
 import net.minecraft.potion.Effect;
 
@@ -15,14 +17,14 @@ public class AntelopeWarHorn extends Item {
         super(properties);
     }
 
-
-    //20 Ticks = 1 second, cooldown duration and effect duration are in ticks
+    // 20 Ticks = 1 second, cooldown duration and effect duration are in ticks
+    public static int COOLDOWN_SEC = 15;
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-
-        playerIn.getCooldownTracker().setCooldown(this, 300);
-        //play sound
+        playerIn.getCooldownTracker().setCooldown(this, COOLDOWN_SEC * 20);
+        // TODO: Exchange sound event with our own, volume probably should be lowered then?
+        worldIn.playSound(null, playerIn.getPosition(), SoundEvents.EVENT_RAID_HORN, SoundCategory.NEUTRAL, 64.0f, 1.0f);
         playerIn.addPotionEffect(new EffectInstance(Effect.get(5), 200, 0));
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
