2edc8262ddb814ec453f79288d325edb6c73d3c1
Paspartout <Paspartout@users.noreply.github.com>
2021-01-30 00:13:17 +0100

    Blopole(#6): FlowerPot rendering POC

--- src/main/java/com/untitledauthors/untitledcreaturemod/creature/blopole/BlopoleEntity.java
+++ src/main/java/com/untitledauthors/untitledcreaturemod/creature/blopole/BlopoleEntity.java
@@ -9,6 +9,8 @@ import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.world.World;
@@ -28,17 +30,15 @@ public class BlopoleEntity extends AnimalEntity implements IAnimatable
     private final AnimationFactory factory = new AnimationFactory(this);
     public static AnimationBuilder IDLE_ANIM = new AnimationBuilder().addAnimation("idle01");
     public static AnimationBuilder WALK_ANIM = new AnimationBuilder().addAnimation("walk");
-//    public static Item BREEDING_ITEM = Items.WHEAT; DECIDE IF CAN BE BRED
+    public static Item BREEDING_ITEM = Items.SEA_PICKLE;
 
     public BlopoleEntity(EntityType<? extends AnimalEntity> type, World worldIn) {
         super(type, worldIn);
     }
 
-    //CHANGE AD MoveSpeed MaxHealth
     public static AttributeModifierMap.MutableAttribute getDefaultAttributes() {
-        return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 5.0D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3F)
-                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 3.0F);
+        return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 7.0D)
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.15D);
     }
 
 
@@ -53,7 +53,7 @@ public class BlopoleEntity extends AnimalEntity implements IAnimatable
         // TODO: Come up with alternative moving predicate?
         //       The default one doesn't seen to work with slow movement speeds.
 
-        boolean isMoving = !(limbSwingAmount > -0.10F) || !(limbSwingAmount < 0.10F);
+        boolean isMoving = !(limbSwingAmount > -0.05) || !(limbSwingAmount < 0.05);
         AnimationBuilder anim = isMoving ? WALK_ANIM : IDLE_ANIM;
         controller.setAnimation(anim);
 
@@ -61,18 +61,20 @@ public class BlopoleEntity extends AnimalEntity implements IAnimatable
     }
 
     protected void registerGoals() {
-
-        ///REDO GOALS
         this.goalSelector.addGoal(0, new SwimGoal(this));
-        this.goalSelector.addGoal(1, new PanicGoal(this, 2.0D));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 1.5D));
         this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-        this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.fromItems(Items.WHEAT), false));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1.00, Ingredient.fromItems(BREEDING_ITEM), false));
         this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25D));
-        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+        this.goalSelector.addGoal(6, new RandomWalkingGoal(this, 1.0D)); // TODO: Lazy random walk?
         this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
     }
 
+    @Override
+    public boolean isBreedingItem(ItemStack stack) {
+        return stack.getItem() == BREEDING_ITEM;
+    }
 
     @Override
     public AnimationFactory getFactory() {
--- src/main/java/com/untitledauthors/untitledcreaturemod/creature/blopole/BlopoleEntityRenderer.java
+++ src/main/java/com/untitledauthors/untitledcreaturemod/creature/blopole/BlopoleEntityRenderer.java
@@ -1,11 +1,60 @@
 package com.untitledauthors.untitledcreaturemod.creature.blopole;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.untitledauthors.untitledcreaturemod.creature.GeoMobRenderer;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 
 public class BlopoleEntityRenderer extends GeoMobRenderer<BlopoleEntity> {
     public BlopoleEntityRenderer(EntityRendererManager renderManager) {
         super(renderManager, new BlopoleEntityModel());
         this.shadowSize = 0.5F;
+    }
+
+    @Override
+    public void renderLate(BlopoleEntity animatable, MatrixStack stack, float ticks, IRenderTypeBuffer renderTypeBuffer, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float partialTicks) {
+        // TODO: Make it render a blockstate owned by the blopole entity
+
+    }
+
+    @Override
+    public void render(BlopoleEntity entity, float entityYaw, float partialTicks, MatrixStack stack, IRenderTypeBuffer bufferIn, int packedLightIn) {
+        super.render(entity, entityYaw, partialTicks, stack, bufferIn, packedLightIn);
+        Minecraft mc = Minecraft.getInstance();
+        stack.push();
+
+        // Field fullPotsField = null;
+        // try {
+        //     fullPotsField = FlowerPotBlock.class.getDeclaredField("fullPots");
+        // } catch (NoSuchFieldException e) {
+        // }
+        // fullPotsField.setAccessible(true);
+        // try {
+        //     Map<ResourceLocation, Supplier<? extends Block>> fullPots = (Map<ResourceLocation, Supplier<? extends Block>>) fullPotsField.get(Blocks.FLOWER_POT);
+        //     fullPots.get(new ResourceLocation("minecraft:poppy"));
+        // } catch (IllegalAccessException e) {
+        //     e.printStackTrace();
+        // }
+
+
+        BlockState state = Blocks.POTTED_POPPY.getDefaultState();
+        IBakedModel flowerPotModel = Minecraft.getInstance().getBlockRendererDispatcher().getModelForState(state);
+        mc.getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+        RenderType type = RenderType.getCutoutMipped();
+        float rotationYaw = MathHelper.interpolateAngle(partialTicks, entity.prevRenderYawOffset, entity.renderYawOffset);
+        float ageInTicks = this.handleRotationFloat(entity, partialTicks);
+        this.applyRotations(entity, stack, ageInTicks, rotationYaw, partialTicks);
+        stack.translate(-0.5f, 0.7f, -0.5f);
+        Minecraft.getInstance().getItemRenderer().renderModel(flowerPotModel, ItemStack.EMPTY, packedLightIn, getPackedOverlay(entity, 0), stack, bufferIn.getBuffer(type));
+        stack.pop();
     }
 }
